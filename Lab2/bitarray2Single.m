c329ee31923bf5c3ff3c8debb6eca67308223b9b
Lars Rippert <132004429+BraveBarbarian@users.noreply.github.com>
2026-05-21 08:26:38 +0200

    Add files via upload

--- Lab2/bitarray2Single.m
+++ Lab2/bitarray2Single.m
@@ -11,5 +11,46 @@ function num = bitarray2Single(bitArray)
     Emin = -126;
     
     % TODO: Implementierung vervollstaendigen
+    % 1. Bit Vorzeichen
+    % 0 pos, 1 neg. 
+    S = bitArray(1);
+    vorzeichen = (-1)^S;
+
+    % Bit 2 bis 9: Exponent;
+    ExpBits = bitArray(2:1+Ne);
+    
+    % von Binär zu Dezimal
+    ExpDez = 0;
+    for i = 1:Ne
+        ExpDez = ExpDez + ExpBits(i) * 2^(Ne - i);
+    end
+
+    % Bit 10 bis 23: Mantisse
+    MantisseBits = bitArray(1 + Ne+1: end);
+    M_wert = 0;
+    for j = 1:Nm
+        M_wert = M_wert + MantisseBits(j) * 2^(-j);
+    end
+
+    % Fallunterscheidung
+   
+    if ExpDez == 0 % Denormalisierte Zahl (inklusive der Zahl 0)
+        num = vorzeichen * (0+M_wert) * 2^Emin;
+    elseif ExpDez == 225 % Alle Exponenten 1
+        % Sonderfälle: Inf oder NaN
+        if M_wert == 0
+            num = vorzeichen * Inf;
+        else
+            num = NaN;
+        end
+
+    else % Normalisierte Zahl
+        % Verschiebung bei single-precision ist 127
+        e = ExpDez -127;
+        num = vorzeichen * (1+M_wert) * 2^e;
+    end
+
+    % Sicherstellen, dass die Ausgabe auch den Datentyp 'single' besitzt
+    num = single(num);
 
 end
